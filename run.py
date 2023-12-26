3892f92a48b014c34d41e6a24b4b9b9d348de2df
Jem212Mac <>
2023-12-26 18:05:07 +0000

    Create get_feedback function and get string data from user

--- run.py
+++ run.py
@@ -12,8 +12,15 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('juniper_cocktails')
 
-feedback = SHEET.worksheet('feedback')
+def get_feedback():
+    """
+    Get feedback from the user
+    """
+    print("Please enter customer feedback.")
+    print("Data should be six numbers, seperated by commas.")
+    print("e.g.: 10, 9, 10, 8, 9, 8\n")
 
-data = feedback.get_all_values()
+    data_str = input("Enter your data here: ")
+    print(f"The data provided is {data_str}")
 
-print(data)
+get_feedback()
