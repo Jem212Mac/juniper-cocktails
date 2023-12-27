7f0bb0c593273b1a8cd4f1108cdc71b849f5a020
Jem212Mac <>
2023-12-27 12:00:18 +0000

    Add welcome text and menu selections

--- run.py
+++ run.py
@@ -12,15 +12,24 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('juniper_cocktails')
 
-def get_feedback():
+print("Welcome to Juniper Cocktails Customer Feedback Application.")
+print("Please use this application to input or amend feedback, on behalf of the customer, from a completed feedback form, or to analyse existing customer feedback.\n")
+print("Please choose from one of the following options:\n")
+
+print("1. Input Customer Feedback.")
+print("2. Amend Customer Feedback.")
+print("3. Delete Customer Feedback.")
+print("4. Analyse All Feedback.")
+print("5. Analyse Feedback By Location.\n")
+
+def input_feedback():
     """
-    Get feedback from the user
+    Input feedback from the customer
     """
-    print("Please enter customer feedback.")
     print("Data should be six numbers, seperated by commas.")
     print("e.g.: 10, 9, 10, 8, 9, 8\n")
 
     data_str = input("Enter your data here:\n")
     print(f"The data provided is {data_str}")
 
-get_feedback()
+input_feedback()
